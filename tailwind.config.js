8d491532ff39b9b9d1606421b0465f149881ccaf
atul-1702 <atulnitish5@gmail.com>
2025-07-07 13:09:56 +0530

    header-completed

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1.5rem",
+          lg: "2rem",
+          xl: "2.5rem",
+          "2xl": "2rem",
+        },
+      },
+    },
+  },
+};
